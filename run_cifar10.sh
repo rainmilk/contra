ec79a150bbb35c1caaf6a111139f85b0dd994a4e
Liang Hu <rainmilk@gmail.com>
2024-10-06 23:21:39 +0800

    update script

--- run_cifar10.sh
+++ run_cifar10.sh
@@ -2,12 +2,12 @@
 export PYTHONPATH = $PYTHONPATH;$(pwd)  #Linux
 
 
-python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epoch 1 --step 1 --learning_rate 0.01 --optimizer adam  --batch_size 256
+python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 1 --step 1 --learning_rate 0.01 --optimizer adam  --batch_size 256
 
-python ./core/train_teacher.py --model cifar-resnet18 --dataset cifar-10 --num_epoch 1 --step 1 --learning_rate 0.005 --optimizer adam  --batch_size 256
+python ./core/train_teacher.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 1 --step 1 --learning_rate 0.005 --optimizer adam  --batch_size 256
 
-python ./core/core.py --model cifar-resnet18 --dataset cifar-10 --num_epoch 1 --step 1 --learning_rate 0.001 --optimizer adam  --batch_size 256
+python ./core/core.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 1 --step 1 --learning_rate 0.001 --optimizer adam  --batch_size 256
 
-python ./core/core.py --model cifar-resnet18 --dataset cifar-10 --num_epoch 1 --step 2 --learning_rate 0.001 --optimizer adam  --batch_size 256
+python ./core/core.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 1 --step 2 --learning_rate 0.001 --optimizer adam  --batch_size 256
 
-python ./core/core.py --model cifar-resnet18 --dataset cifar-10 --num_epoch 1 --step 3 --learning_rate 0.001 --optimizer adam  --batch_size 256
+python ./core/core.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 1 --step 3 --learning_rate 0.001 --optimizer adam  --batch_size 256
