78b78fcc546869606a79a71b281186843d690ba9
suizhihao <suizhihao@pjlab.org.cn>
2024-10-07 11:00:34 +0800

    update codes

--- run_cifar10_contra.sh
+++ run_cifar10_contra.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+# 检查是否传递了GPU的参数
+if [ -z "$1" ]; then
+    echo "使用方法: ./run_cifar_replay.sh <GPU_ID>"
+    exit 1
+fi
+
+# 设置指定的 GPU
+export CUDA_VISIBLE_DEVICES=$1
+echo "Using GPU $1"
+echo "CUDA_VISIBLE_DEVICES is set to: $CUDA_VISIBLE_DEVICES"
+
 # $env:PYTHONPATH += ($pwd).Path  # Powershell
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 echo "PYTHONPATH is set to: $PYTHONPATH"
