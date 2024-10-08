e7ed338657f9fb5223a225e77b2a8daff9f3675f
suizhihao <suizhihao@pjlab.org.cn>
2024-10-08 20:22:16 +0800

    update codes

--- run_pet37_replay_asy.sh
+++ run_pet37_replay_asy.sh
@@ -15,10 +15,10 @@ echo "CUDA_VISIBLE_DEVICES is set to: $CUDA_VISIBLE_DEVICES"
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 echo "PYTHONPATH is set to: $PYTHONPATH"
 
-python ./run_experiment.py --model wideresnet50 --dataset pet-37 --num_epochs 1 --step 0 --learning_rate 0.01 --optimizer adam --batch_size 256 --uni_name replay --balanced --noise_type asymmetric
+python ./run_experiment.py --model wideresnet50 --dataset pet-37 --num_epochs 1 --step 0 --learning_rate 0.01 --optimizer adam --batch_size 256 --uni_name replay --balanced --noise_type asymmetric --model_suffix worker_restore
 
-python ./run_experiment.py --model wideresnet50 --dataset pet-37 --num_epochs 1 --step 1 --learning_rate 0.01 --optimizer adam --batch_size 256 --uni_name replay --balanced --noise_type asymmetric
+python ./run_experiment.py --model wideresnet50 --dataset pet-37 --num_epochs 1 --step 1 --learning_rate 0.01 --optimizer adam --batch_size 256 --uni_name replay --balanced --noise_type asymmetric --model_suffix worker_restore
 
-python ./run_experiment.py --model wideresnet50 --dataset pet-37 --num_epochs 1 --step 2 --learning_rate 0.01 --optimizer adam --batch_size 256 --uni_name replay --balanced --noise_type asymmetric
+python ./run_experiment.py --model wideresnet50 --dataset pet-37 --num_epochs 1 --step 2 --learning_rate 0.01 --optimizer adam --batch_size 256 --uni_name replay --balanced --noise_type asymmetric --model_suffix worker_restore
 
-python ./run_experiment.py --model wideresnet50 --dataset pet-37 --num_epochs 1 --step 3 --learning_rate 0.01 --optimizer adam --batch_size 256 --uni_name replay --balanced
+python ./run_experiment.py --model wideresnet50 --dataset pet-37 --num_epochs 1 --step 3 --learning_rate 0.01 --optimizer adam --batch_size 256 --uni_name replay --balanced --model_suffix worker_restore
