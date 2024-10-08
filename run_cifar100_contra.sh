8789405260f4c359617dc7fd35c9be8e52485e8b
suizhihao <suizhihao@pjlab.org.cn>
2024-10-08 14:30:54 +0800

    update codes

--- run_cifar100_contra.sh
+++ run_cifar100_contra.sh
@@ -15,18 +15,18 @@ echo "CUDA_VISIBLE_DEVICES is set to: $CUDA_VISIBLE_DEVICES"
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 echo "PYTHONPATH is set to: $PYTHONPATH"
 
-python ./run_experiment.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 0 --learning_rate 0.01 --optimizer adam --batch_size 256 --balanced
+python ./run_experiment.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 0 --learning_rate 0.01 --optimizer adam --batch_size 256 --balanced --noise_type asymmetric
 
-python ./core_model/train_teacher.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 0 --learning_rate 0.005 --optimizer adam --batch_size 256 --balanced
+python ./core_model/train_teacher.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 0 --learning_rate 0.005 --optimizer adam --batch_size 256 --balanced --noise_type asymmetric
 
-python ./run_experiment.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 1 --learning_rate 0.01 --optimizer adam --batch_size 256 --balanced --model_suffix worker_raw
+python ./run_experiment.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 1 --learning_rate 0.01 --optimizer adam --batch_size 256 --balanced --model_suffix worker_raw --noise_type asymmetric
 
-python ./core_model/core.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 1 --learning_rate 0.001 --optimizer adam --batch_size 256 --balanced
+python ./core_model/core.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 1 --learning_rate 0.001 --optimizer adam --batch_size 256 --balanced --noise_type asymmetric
 
-python ./run_experiment.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 2 --learning_rate 0.01 --optimizer adam --batch_size 256 --balanced --model_suffix worker_raw
+python ./run_experiment.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 2 --learning_rate 0.01 --optimizer adam --batch_size 256 --balanced --model_suffix worker_raw --noise_type asymmetric
 
-python ./core_model/core.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 2 --learning_rate 0.001 --optimizer adam --batch_size 256 --balanced
+python ./core_model/core.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 2 --learning_rate 0.001 --optimizer adam --batch_size 256 --balanced --noise_type asymmetric
 
-python ./run_experiment.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 3 --learning_rate 0.01 --optimizer adam --batch_size 256 --balanced --model_suffix worker_raw
+python ./run_experiment.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 3 --learning_rate 0.01 --optimizer adam --batch_size 256 --balanced --model_suffix worker_raw --noise_type asymmetric
 
-python ./core_model/core.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 3 --learning_rate 0.001 --optimizer adam --batch_size 256 --balanced
+python ./core_model/core.py --model cifar-wideresnet40 --dataset cifar-100 --num_epochs 1 --step 3 --learning_rate 0.001 --optimizer adam --batch_size 256 --balanced --noise_type asymmetric
