b418e79c2bd6b5cce3054dfe516d307aed40a3d9
frederickszk <szk037@163.com>
2024-10-10 15:31:47 +0800

    修改raw组脚本适配run_experiment.py。raw组：不使用任何修复或者tta纯训

--- run_pet37_raw_asy.sh
+++ run_pet37_raw_asy.sh
@@ -15,6 +15,11 @@ echo "CUDA_VISIBLE_DEVICES is set to: $CUDA_VISIBLE_DEVICES"
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 echo "PYTHONPATH is set to: $PYTHONPATH"
 
+# [24-10-10, Add by sunzekun]
+# 由于run_experiment.py的逻辑发生变化，现在要先不指定--uni_name，生成一次存储在/pretrain/step_0的模型，才能正常执行后续的操作
+# 如果通过其它指令，在对应组下有/pretrain/step_0 的模型，可以注释掉第一行。
+python ./run_experiment.py --model wideresnet50 --dataset pet-37 --num_epochs 20 --step 0 --learning_rate 0.0001 --optimizer adam --batch_size 64 --balanced --noise_type asymmetric --model_suffix worker_restore
+
 python ./run_experiment.py --model wideresnet50 --dataset pet-37 --num_epochs 20 --step 0 --learning_rate 0.0001 --optimizer adam --batch_size 64 --uni_name raw --balanced --noise_type asymmetric --model_suffix worker_restore
 
 python ./run_experiment.py --model wideresnet50 --dataset pet-37 --num_epochs 20 --step 1 --learning_rate 0.0001 --optimizer adam --batch_size 64 --uni_name raw --balanced --noise_type asymmetric --model_suffix worker_restore
