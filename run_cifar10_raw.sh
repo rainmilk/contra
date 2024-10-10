5cf0ed70d4219ce03ed7f4cdfb7fe649d1ed75bc
frederickszk <szk037@163.com>
2024-10-10 15:34:53 +0800

    修改raw组脚本

--- run_cifar10_raw.sh
+++ run_cifar10_raw.sh
@@ -21,7 +21,7 @@ export PYTHONPATH=$PYTHONPATH:$(pwd)
 echo "PYTHONPATH is set to: $PYTHONPATH"
 
 
-# [24-10-09, Add by sunzekun]
+# [24-10-10, Add by sunzekun]
 # 由于run_experiment.py的逻辑发生变化，现在要先不指定--uni_name，生成一次存储在/pretrain/step_0的模型，才能正常执行后续的操作
 # 如果通过其它指令，在对应组下有/pretrain/step_0 的模型，可以注释掉第一行。
 python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 0 --learning_rate 0.01 --optimizer adam --batch_size 256 --balanced --model_suffix worker_restore
