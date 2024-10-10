9d6c2d60ce52b516a8634b6771137239bb8576bc
suizhihao <suizhihao@pjlab.org.cn>
2024-10-10 16:55:20 +0800

    update codes

--- train_basic_models.sh
+++ train_basic_models.sh
@@ -1,6 +1,6 @@
 mkdir -p logs
 
-nohup bash -c "CUDA_VISIBLE_DEVICES=1 python run_experiment.py \
+nohup bash -c "CUDA_VISIBLE_DEVICES=4 python run_experiment.py \
     --step 0 \
     --model cifar-resnet18 \
     --dataset cifar-10 \
@@ -12,7 +12,7 @@ nohup bash -c "CUDA_VISIBLE_DEVICES=1 python run_experiment.py \
     --optimizer adam \
     --batch_size 256" >logs/run_experiment_step0_resnet18_cifar10_$(date +'%Y%m%d_%H%M%S').log 2>&1 &
 
-nohup bash -c "CUDA_VISIBLE_DEVICES=2 python run_experiment.py \
+nohup bash -c "CUDA_VISIBLE_DEVICES=5 python run_experiment.py \
     --step 0 \
     --model wideresnet50 \
     --dataset pet-37 \
@@ -24,7 +24,7 @@ nohup bash -c "CUDA_VISIBLE_DEVICES=2 python run_experiment.py \
     --optimizer adam \
     --batch_size 64" >logs/run_experiment_step0_wideresnet50_pet37_$(date +'%Y%m%d_%H%M%S').log 2>&1 &
 
-nohup bash -c "CUDA_VISIBLE_DEVICES=3 python run_experiment.py \
+nohup bash -c "CUDA_VISIBLE_DEVICES=6 python run_experiment.py \
     --step 0 \
     --model cifar-wideresnet40 \
     --dataset cifar-100 \
@@ -36,14 +36,14 @@ nohup bash -c "CUDA_VISIBLE_DEVICES=3 python run_experiment.py \
     --optimizer adam \
     --batch_size 256" >logs/run_experiment_step0_wideresnet40_cifar100_$(date +'%Y%m%d_%H%M%S').log 2>&1 &
 
-#nohup bash -c "CUDA_VISIBLE_DEVICES=4 python run_experiment.py \
-#    --step 0 \
-#    --model wideresnet50 \
-#    --dataset pet-37 \
-#    --noise_ratio 0.2 \
-#    --noise_type asymmetric \
-#    --balanced \
-#    --num_epochs 30 \
-#    --learning_rate 0.0001 \
-#    --optimizer adam \
-#    --batch_size 64" >logs/run_experiment_step0_wideresnet50_pet37_asymmetric_$(date +'%Y%m%d_%H%M%S').log 2>&1 &
+nohup bash -c "CUDA_VISIBLE_DEVICES=7 python run_experiment.py \
+   --step 0 \
+   --model wideresnet50 \
+   --dataset pet-37 \
+   --noise_ratio 0.2 \
+   --noise_type asymmetric \
+   --balanced \
+   --num_epochs 30 \
+   --learning_rate 0.0001 \
+   --optimizer adam \
+   --batch_size 64" >logs/run_experiment_step0_wideresnet50_pet37_asymmetric_$(date +'%Y%m%d_%H%M%S').log 2>&1 &
