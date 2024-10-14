8e030495f5ac35af59b408b1edecfcb020c0569f
Liang Hu <rainmilk@gmail.com>
2024-10-14 21:44:59 +0800

    update training

--- run_cifar10_coteaching_plus.sh
+++ run_cifar10_coteaching_plus.sh
@@ -15,17 +15,17 @@ echo "CUDA_VISIBLE_DEVICES is set to: $CUDA_VISIBLE_DEVICES"
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 echo "PYTHONPATH is set to: $PYTHONPATH"
 
-python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 0 --batch_size 16 --balanced --uni_name Coteachingplus --learning_rate 1e-4
+python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 0 --batch_size 16 --balanced --uni_name Coteachingplus --learning_rate 5e-3
 
-python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 1 --batch_size 16 --balanced --model_suffix worker_raw --uni_name Coteachingplus --learning_rate 1e-4
+python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 1 --batch_size 16 --balanced --model_suffix worker_raw --uni_name Coteachingplus --learning_rate 5e-3
 
-python baseline_code/colearn/main.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 1 --batch_size 16 --balanced --uni_name Coteachingplus --learning_rate 1e-4
+python baseline_code/colearn/main.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 1 --batch_size 16 --balanced --uni_name Coteachingplus --learning_rate 5e-3
 
-python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 2 --batch_size 16 --balanced --model_suffix worker_raw --uni_name Coteachingplus --learning_rate 1e-4
+python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 2 --batch_size 16 --balanced --model_suffix worker_raw --uni_name Coteachingplus --learning_rate 5e-3
 
-python baseline_code/colearn/main.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 2 --batch_size 16 --balanced --uni_name Coteachingplus --learning_rate 1e-4
+python baseline_code/colearn/main.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 2 --batch_size 16 --balanced --uni_name Coteachingplus --learning_rate 5e-3
 
-python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 3 --batch_size 16 --balanced --model_suffix worker_raw --uni_name Coteachingplus --learning_rate 1e-4
+python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 3 --batch_size 16 --balanced --model_suffix worker_raw --uni_name Coteachingplus --learning_rate 5e-3
 
-python baseline_code/colearn/main.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 3 --batch_size 16 --balanced --uni_name Coteachingplus --learning_rate 1e-4
+python baseline_code/colearn/main.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 3 --batch_size 16 --balanced --uni_name Coteachingplus --learning_rate 5e-3
 
