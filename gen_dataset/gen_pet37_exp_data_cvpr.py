6c62787b6af29869ff9952f1703c42382ec6aa63
suizhihao <suizhihao@pjlab.org.cn>
2024-10-30 14:58:08 +0800

    add pet-37 experimental dataset for cvpr

--- gen_dataset/gen_pet37_exp_data_cvpr.py
+++ gen_dataset/gen_pet37_exp_data_cvpr.py
@@ -8,10 +8,10 @@ import torchvision
 from torchvision import datasets, transforms
 import json
 
-import collections
 from configs import settings
-from split_dataset import split
+from gen_dataset.split_dataset import split_data
 
+conference_name = "cvpr"
 
 def load_classes_from_file(file_path):
     """从文件中读取类别列表"""
@@ -48,9 +48,7 @@ def build_asymmetric_mapping(superclass_mapping, classes, rng):
                 new_class = rng.choice(available_classes)
                 asymmetric_mapping[class_name] = new_class
             else:
-                asymmetric_mapping[class_name] = (
-                    class_name  # 如果没有其他类别，则保持原标签不变
-                )
+                asymmetric_mapping[class_name] = class_name
     return asymmetric_mapping
 
 
@@ -59,15 +57,11 @@ def create_pet37_npy_files(
     gen_dir,
     noise_type="symmetric",
     noise_ratio=0.2,
-    num_versions=3,
-    retention_ratios=[0.5, 0.3, 0.1],
-    balanced=False,
+    split_ratio=0.5,
 ):
-
     rng = np.random.default_rng(42)
 
     weights = torchvision.models.ResNet18_Weights.DEFAULT
-
     data_transform = transforms.Compose([weights.transforms()])
 
     # 加载 PET-37 数据集
@@ -78,162 +72,77 @@ def create_pet37_npy_files(
         root=data_dir, split="test", download=True, transform=data_transform
     )
 
-    train_data, train_labels = zip(*train_dataset)
-    train_data = torch.stack(train_data)
-    train_labels = torch.tensor(train_labels)
-
-    test_data, test_labels = zip(*test_dataset)
-    test_data = torch.stack(test_data)
-    test_labels = torch.tensor(test_labels)
-
-    case = settings.get_case(noise_ratio, noise_type, balanced)
-    print("使用类均衡的数据划分方式...")
+    print("划分训练集...")
     dataset_name = "pet-37"
     num_classes = 37
-    D_inc_data, D_inc_labels = split(dataset_name, case, train_dataset, test_dataset, num_classes)
+    D_inc_data, D_inc_labels = split_data(
+        dataset_name, train_dataset, test_dataset, num_classes, split_ratio
+    )
 
-    # 读取 Oxford-Pets 类别
-    pet37_classes_file = os.path.join(settings.root_dir, "configs/classes/pet_37_classes.txt")
+    pet37_classes_file = os.path.join(
+        settings.root_dir, "configs/classes/pet_37_classes.txt"
+    )
     pet37_classes = load_classes_from_file(pet37_classes_file)
 
-    # 打印读取到的类别信息
-    print("PET-37 Classes:", pet37_classes)
-
-    # 读取 Oxford-Pets 的 superclass 和 child class 映射
-    pet37_mapping_file = os.path.join(settings.root_dir, "configs/classes/pet_37_mapping.json")
+    pet37_mapping_file = os.path.join(
+        settings.root_dir, "configs/classes/pet_37_mapping.json"
+    )
     pet37_superclass_mapping = load_pet37_superclass_mapping(pet37_mapping_file)
 
-    # 定义遗忘类别和噪声类别
-    forget_classes = list(range(18))  # 前18个类别作为遗忘类别
-    noise_classes = list(range(18, 28))  # 接下来的10个类别作为噪声类别
+    print("PET-37 Classes:", pet37_classes)
 
-    # 获取增量数据集中的遗忘类别和噪声类别的索引
-    D_inc_forget_indices = [
-        i for i in range(len(D_inc_labels)) if D_inc_labels[i] in forget_classes
-    ]
-    D_inc_noise_indices = [
-        i for i in range(len(D_inc_labels)) if D_inc_labels[i] in noise_classes
-    ]
-
-    # 构建非对称映射，如果选择了非对称噪声
     if noise_type == "asymmetric":
         asymmetric_mapping = build_asymmetric_mapping(
             pet37_superclass_mapping, pet37_classes, rng
         )
 
-    symmetric_noisy_classes = []
-    asymmetric_noisy_classes = []
-    symmetric_noisy_classes_simple = set()
-    asymmetric_noisy_classes_simple = set()
+    num_noisy_samples = int(len(D_inc_labels) * noise_ratio)
+    noisy_indices = np.random.choice(
+        len(D_inc_labels), num_noisy_samples, replace=False
+    )
+    noisy_sel = np.zeros(len(D_inc_labels), dtype=np.bool_)
+    noisy_sel[noisy_indices] = True
 
-    rng = np.random.default_rng(42)
-    for t in range(num_versions):
-        retention_ratio = retention_ratios[t]
-        num_forget_samples = int(len(D_inc_forget_indices) * retention_ratio)
-        if num_forget_samples > 0:
-            forget_sample_indices = rng.choice(
-                D_inc_forget_indices, num_forget_samples, replace=False
-            )
-            D_f_data = D_inc_data[forget_sample_indices]
-            D_f_labels = D_inc_labels[forget_sample_indices]
-        else:
-            D_f_data = torch.empty(0, 3, 224, 224)
-            D_f_labels = torch.empty(0, dtype=torch.long)
-
-        noise_sample_indices = D_inc_noise_indices
-        num_noisy_samples = int(len(noise_sample_indices) * noise_ratio)
-        if num_noisy_samples > 0:
-            noisy_indices = rng.choice(
-                noise_sample_indices, num_noisy_samples, replace=False
-            )
-        else:
-            noisy_indices = []
-
-        D_n_data = D_inc_data[noise_sample_indices]
-        D_n_labels = D_inc_labels[noise_sample_indices]
-
-        for idx_in_D_n, D_inc_idx in enumerate(noise_sample_indices):
-            if D_inc_idx in noisy_indices:
-                original_label = int(D_n_labels[idx_in_D_n].item())
-                if original_label in noise_classes:
-                    original_class_name = pet37_classes[original_label]
-                    if noise_type == "symmetric":
-                        new_label = rng.choice(
-                            [i for i in range(num_classes) if i != original_label]
-                        )
-                        D_n_labels[idx_in_D_n] = new_label
-                        symmetric_noisy_classes.append(
-                            {
-                                "original_label": int(original_label),
-                                "original_class_name": original_class_name,
-                                "new_label": int(new_label),
-                                "new_class_name": pet37_classes[new_label],
-                            }
-                        )
-                        symmetric_noisy_classes_simple.add(
-                            (int(original_label), int(new_label))
-                        )
-                    elif noise_type == "asymmetric":
-                        if original_class_name in asymmetric_mapping:
-                            new_class_name = asymmetric_mapping[original_class_name]
-                            new_label = pet37_classes.index(new_class_name)
-                            D_n_labels[idx_in_D_n] = new_label
-                            asymmetric_noisy_classes.append(
-                                {
-                                    "original_label": int(original_label),
-                                    "original_class_name": original_class_name,
-                                    "new_label": int(new_label),
-                                    "new_class_name": new_class_name,
-                                }
-                            )
-                            asymmetric_noisy_classes_simple.add(
-                                (int(original_label), int(new_label))
-                            )
-                    else:
-                        raise ValueError("Invalid noise type.")
-
-        D_tr_data = np.concatenate([D_f_data, D_n_data], axis=0)
-        D_tr_labels = np.concatenate([D_f_labels, D_n_labels], axis=0)
-        perm = rng.permutation(len(D_tr_data))
-        D_tr_data = D_tr_data[perm]
-        D_tr_labels = D_tr_labels[perm]
-
-        train_data_path = settings.get_dataset_path(
-            dataset_name, case, "train_data", t + 1
-        )
-        train_label_path = settings.get_dataset_path(
-            dataset_name, case, "train_label", t + 1
-        )
-        subdir = os.path.dirname(train_data_path)
-        os.makedirs(subdir, exist_ok=True)
-        np.save(train_data_path, D_tr_data)
-        np.save(train_label_path, D_tr_labels)
-        print(f"D_tr 版本 {t + 1} 已保存到 {subdir}")
+    D_noisy_data = D_inc_data[noisy_sel]
+    D_noisy_true_labels = D_inc_labels[noisy_sel]
+    D_normal_data = D_inc_data[~noisy_sel]
+    D_normal_labels = D_inc_labels[~noisy_sel]
 
     if noise_type == "symmetric":
-        with open(
-            f"{dataset_name}-{noise_type}-{noise_ratio}-symmetric_noisy_classes_detailed.json",
-            "w",
-        ) as f:
-            json.dump(symmetric_noisy_classes, f, indent=4)
-        with open(
-            f"{dataset_name}-{noise_type}-{noise_ratio}-symmetric_noisy_classes_simple.json",
-            "w",
-        ) as f:
-            json.dump(list(symmetric_noisy_classes_simple), f, indent=4)
+        D_noisy_labels = np.random.choice(num_classes, num_noisy_samples, replace=True)
     elif noise_type == "asymmetric":
-        with open(
-            f"{dataset_name}-{noise_type}-{noise_ratio}-asymmetric_noisy_classes_detailed.json",
-            "w",
-        ) as f:
-            json.dump(asymmetric_noisy_classes, f, indent=4)
-        with open(
-            f"{dataset_name}-{noise_type}-{noise_ratio}-asymmetric_noisy_classes_simple.json",
-            "w",
-        ) as f:
-            json.dump(list(asymmetric_noisy_classes_simple), f, indent=4)
+        D_noisy_labels = []
+        for true_label in D_noisy_true_labels:
+            original_class_name = pet37_classes[true_label]
+            if original_class_name in asymmetric_mapping:
+                new_class_name = asymmetric_mapping[original_class_name]
+                new_label = pet37_classes.index(new_class_name)
+            else:
+                new_label = true_label
+            D_noisy_labels.append(new_label)
+        D_noisy_labels = np.array(D_noisy_labels)
+    else:
+        raise ValueError("Invalid noise type.")
 
-    print("所有数据集生成完毕。")
+    save_path = os.path.join(
+        gen_dir, f"nr_{noise_ratio}_nt_{noise_type}_{conference_name}"
+    )
+    os.makedirs(save_path, exist_ok=True)
+
+
+    D_1_minus_data_path = os.path.join(save_path, "train_clean_data.npy")
+    D_1_minus_labels_path = os.path.join(save_path, "train_clean_labels.npy")
+    np.save(D_1_minus_data_path, np.array(D_normal_data))
+    np.save(D_1_minus_labels_path, np.array(D_normal_labels))
+
+    D_1_plus_data_path = os.path.join(save_path, "train_noisy_data.npy")
+    D_1_plus_labels_path = os.path.join(save_path, "train_noisy_labels.npy")
+    D_1_plus_true_labels_path = os.path.join(save_path, "train_noisy_true_labels.npy")
+    np.save(D_1_plus_data_path, np.array(D_noisy_data))
+    np.save(D_1_plus_labels_path, np.array(D_noisy_labels))
+    np.save(D_1_plus_true_labels_path, np.array(D_noisy_true_labels))
+
+    print("D_0、D_1_minus 和 D_1_plus 数据集已生成并保存。")
 
 
 def main():
@@ -241,7 +150,7 @@ def main():
     torch.manual_seed(42)
 
     parser = argparse.ArgumentParser(
-        description="Generate PET-37 incremental datasets."
+        description="Generate PET-37 experimental datasets."
     )
     parser.add_argument(
         "--data_dir",
@@ -256,29 +165,27 @@ def main():
         help="生成数据集的保存目录",
     )
     parser.add_argument(
+        "--dataset_name",
+        type=str,
+        choices=["pet-37"],
+        default="pet-37",
+        help="数据集仅支持：'pet-37'",
+    )
+    parser.add_argument(
+        "--split_ratio",
+        type=float,
+        default=0.5,
+        help="训练集划分比例（默认 0.5）",
+    )
+    parser.add_argument(
         "--noise_type",
         type=str,
         choices=["symmetric", "asymmetric"],
         default="symmetric",
-        help="标签噪声类型：'symmetric' 或 'asymmetric'",
+        help="标签器歪差类型：'symmetric' 或 'asymmetric'",
     )
     parser.add_argument(
-        "--noise_ratio", type=float, default=0.2, help="噪声比例（默认 0.2）"
-    )
-    parser.add_argument(
-        "--num_versions", type=int, default=3, help="生成的增量版本数量（默认 3）"
-    )
-    parser.add_argument(
-        "--retention_ratios",
-        type=float,
-        nargs="+",
-        default=[0.5, 0.3, 0.1],
-        help="各增量版本的保留比例列表",
-    )
-    parser.add_argument(
-        "--balanced",
-        action="store_true",
-        help="是否使用类均衡的数据划分方式。如果不指定，则使用随机划分。",
+        "--noise_ratio", type=float, default=0.2, help="噪音比例（默认 0.2）"
     )
 
     args = parser.parse_args()
@@ -288,9 +195,7 @@ def main():
         gen_dir=args.gen_dir,
         noise_type=args.noise_type,
         noise_ratio=args.noise_ratio,
-        num_versions=args.num_versions,
-        retention_ratios=args.retention_ratios,
-        balanced=args.balanced,
+        split_ratio=args.split_ratio,
     )
 
 
