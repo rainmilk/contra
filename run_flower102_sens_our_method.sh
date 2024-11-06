fb6c3eafcb4921aab6794ea60b2572559855ee32
suizhihao <suizhihao@pjlab.org.cn>
2024-11-06 14:17:57 +0800

    refine scripts

--- run_flower102_sens_our_method.sh
+++ run_flower102_sens_our_method.sh
@@ -20,11 +20,17 @@ the following command has been disused
 '''
 # python ./core_model/CRUL.py --model wideresnet50 --dataset flower-102 --num_epochs 3 --repair_iter_num 5 --uni_name CRUL --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0
 
-python ./core_model/CRUL.py --model wideresnet50 --dataset flower-102 --num_epochs 3 --repair_iter_num 5 --uni_name CRUL --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.25
+# noise 0.25
+python ./core_model/CRUL.py --model wideresnet50 --dataset flower-102 --num_epochs 3 --repair_iter_num 5 --uni_name CRUL --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.25 --mixup_alpha 0.5
+# CUDA_VISIBLE_DEVICES=4 python ./core_model/CRUL.py --model wideresnet50 --dataset flower-102 --num_epochs 3 --repair_iter_num 5 --uni_name CRUL --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.25 --mixup_alpha 0.5
 
 '''
 the following command has been executed in batch_run_our_method_cvpr.sh
 '''
-# python ./core_model/CRUL.py --model wideresnet50 --dataset flower-102 --num_epochs 3 --repair_iter_num 5 --uni_name CRUL --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.5
+# noise 0.5
+# python ./core_model/CRUL.py --model wideresnet50 --dataset flower-102 --num_epochs 3 --repair_iter_num 5 --uni_name CRUL --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.5 --mixup_alpha 0.5
+# CUDA_VISIBLE_DEVICES=5 python ./core_model/CRUL.py --model wideresnet50 --dataset flower-102 --num_epochs 3 --repair_iter_num 5 --uni_name CRUL --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.5 --mixup_alpha 0.5
 
-python ./core_model/CRUL.py --model wideresnet50 --dataset flower-102 --num_epochs 3 --repair_iter_num 5 --uni_name CRUL --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.75
+# noise 0.75
+python ./core_model/CRUL.py --model wideresnet50 --dataset flower-102 --num_epochs 3 --repair_iter_num 5 --uni_name CRUL --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.75 --mixup_alpha 0.5
+# CUDA_VISIBLE_DEVICES=6 python ./core_model/CRUL.py --model wideresnet50 --dataset flower-102 --num_epochs 3 --repair_iter_num 5 --uni_name CRUL --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.75 --mixup_alpha 0.5
--- run_pet37_sens_lnl.sh
+++ run_pet37_sens_lnl.sh
@@ -17,26 +17,13 @@ echo "PYTHONPATH is set to: $PYTHONPATH"
 
 # lnl
 ## best lnl method: ex.ct
-python ./baseline_code/colearn/main_cvpr.py --model wideresnet50 --dataset pet-37 --num_epochs 10 --uni_name Coteaching --learning_rate 1e-4 --optimizer adam --batch_size 64 --noise_type asymmetric --noise_ratio 0
-# python ./baseline_code/colearn/main_cvpr.py --model wideresnet50 --dataset pet-37 --num_epochs 10 --uni_name Coteaching --learning_rate 1e-4 --optimizer adam --batch_size 64 --noise_type asymmetric --noise_ratio 0.25
+# python ./baseline_code/colearn/main_cvpr.py --model wideresnet50 --dataset pet-37 --num_epochs 10 --uni_name Coteaching --learning_rate 1e-4 --optimizer adam --batch_size 64 --noise_type asymmetric --noise_ratio 0
+python ./baseline_code/colearn/main_cvpr.py --model wideresnet50 --dataset pet-37 --num_epochs 10 --uni_name Coteaching --learning_rate 1e-4 --optimizer adam --batch_size 64 --noise_type asymmetric --noise_ratio 0.25
 python ./baseline_code/colearn/main_cvpr.py --model wideresnet50 --dataset pet-37 --num_epochs 10 --uni_name Coteaching --learning_rate 1e-4 --optimizer adam --batch_size 64 --noise_type asymmetric --noise_ratio 0.5
 python ./baseline_code/colearn/main_cvpr.py --model wideresnet50 --dataset pet-37 --num_epochs 10 --uni_name Coteaching --learning_rate 1e-4 --optimizer adam --batch_size 64 --noise_type asymmetric --noise_ratio 0.75
+
 ## second best  lnl method: ex.ct+
-python ./baseline_code/colearn/main_cvpr.py --model wideresnet50 --dataset pet-37 --num_epochs 10 --uni_name Coteachingplus --learning_rate 1e-4 --optimizer adam --batch_size 64 --noise_type asymmetric --noise_ratio 0
-# python ./baseline_code/colearn/main_cvpr.py --model wideresnet50 --dataset pet-37 --num_epochs 10 --uni_name Coteachingplus --learning_rate 1e-4 --optimizer adam --batch_size 64 --noise_type asymmetric --noise_ratio 0.25
+# python ./baseline_code/colearn/main_cvpr.py --model wideresnet50 --dataset pet-37 --num_epochs 10 --uni_name Coteachingplus --learning_rate 1e-4 --optimizer adam --batch_size 64 --noise_type asymmetric --noise_ratio 0
+python ./baseline_code/colearn/main_cvpr.py --model wideresnet50 --dataset pet-37 --num_epochs 10 --uni_name Coteachingplus --learning_rate 1e-4 --optimizer adam --batch_size 64 --noise_type asymmetric --noise_ratio 0.25
 python ./baseline_code/colearn/main_cvpr.py --model wideresnet50 --dataset pet-37 --num_epochs 10 --uni_name Coteachingplus --learning_rate 1e-4 --optimizer adam --batch_size 64 --noise_type asymmetric --noise_ratio 0.5
 python ./baseline_code/colearn/main_cvpr.py --model wideresnet50 --dataset pet-37 --num_epochs 10 --uni_name Coteachingplus --learning_rate 1e-4 --optimizer adam --batch_size 64 --noise_type asymmetric --noise_ratio 0.75
-
-# python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 0 --batch_size 32 --balanced --uni_name Coteaching --learning_rate 5e-3
-
-# python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 1 --batch_size 32 --balanced --model_suffix worker_raw --uni_name Coteaching --learning_rate 5e-3
-
-# python baseline_code/colearn/main.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 1 --batch_size 32 --balanced --uni_name Coteaching --learning_rate 5e-3
-
-# python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 2 --batch_size 32 --balanced --model_suffix worker_raw --uni_name Coteaching --learning_rate 5e-3
-
-# python baseline_code/colearn/main.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 2 --batch_size 32 --balanced --uni_name Coteaching --learning_rate 5e-3
-
-# python ./run_experiment.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 3 --batch_size 32 --balanced --model_suffix worker_raw --uni_name Coteaching --learning_rate 5e-3
-
-# python baseline_code/colearn/main.py --model cifar-resnet18 --dataset cifar-10 --num_epochs 10 --step 3 --batch_size 32 --balanced --uni_name Coteaching --learning_rate 5e-3
