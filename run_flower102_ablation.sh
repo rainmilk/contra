147a0926541622f52ca742dc2e38dc120a4c6451
suizhihao <suizhihao@pjlab.org.cn>
2024-11-11 12:01:59 +0800

    refine abl script

--- run_flower102_ablation.sh
+++ run_flower102_ablation.sh
@@ -19,10 +19,13 @@ echo "PYTHONPATH is set to: $PYTHONPATH"
 python ./core_model/CRUL.py --uni_name CRUL_full --model wideresnet50 --dataset flower-102 --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.5 --repair_iter_num 3 --ls_gamma 0.3 --num_epochs 3 --ul_epochs 2 --agree_epochs 3
 
 # w/o Unlearning
-python ./core_model/CRUL.py --uni_name CRUL_ul --model wideresnet50 --dataset flower-102 --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.5 --repair_iter_num 3 --ls_gamma 0.3 --num_epochs 3 --ul_epochs 0 --agree_epochs 3
+python ./core_model/CRUL.py --uni_name CRUL_wo_ul --model wideresnet50 --dataset flower-102 --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.5 --repair_iter_num 3 --ls_gamma 0.3 --num_epochs 3 --ul_epochs 0 --agree_epochs 3
 
 # w/o High-confidence agreement label smoothing
-python ./core_model/CRUL.py --uni_name CRUL_ls --model wideresnet50 --dataset flower-102 --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.5 --repair_iter_num 3 --ls_gamma 0.3 --num_epochs 3 --ul_epochs 2 --agree_epochs 0
+python ./core_model/CRUL.py --uni_name CRUL_wo_ls --model wideresnet50 --dataset flower-102 --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.5 --repair_iter_num 3 --ls_gamma 0.3 --num_epochs 3 --ul_epochs 2 --agree_epochs 0
 
 # w/o Unlearning and High-confidence agreement label smoothing
-python ./core_model/CRUL.py --uni_name CRUL_ullc --model wideresnet50 --dataset flower-102 --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.5 --repair_iter_num 3 --ls_gamma 0.3 --num_epochs 3 --ul_epochs 0 --agree_epochs 0
+python ./core_model/CRUL.py --uni_name CRUL_wo_ulls --model wideresnet50 --dataset flower-102 --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.5 --repair_iter_num 3 --ls_gamma 0.3 --num_epochs 3 --ul_epochs 0 --agree_epochs 0
+
+# w/o mixup
+python ./core_model/CRUL.py --uni_name CRUL_wo_mixup --model wideresnet50 --dataset flower-102 --learning_rate 1e-4 --optimizer adam --batch_size 256 --noise_type symmetric --noise_ratio 0.5 --repair_iter_num 3 --num_epochs 3 --ul_epochs 2 --agree_epochs 3 --mixup_alpha 0.001 --ls_gamma=0.2 --temperature=0.8
