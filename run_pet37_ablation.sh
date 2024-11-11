69f218cc1907399eb5198e920065b6e704571ed1
suizhihao <suizhihao@pjlab.org.cn>
2024-11-11 17:19:34 +0800

    refine

--- run_pet37_ablation.sh
+++ run_pet37_ablation.sh
@@ -19,7 +19,7 @@ echo "PYTHONPATH is set to: $PYTHONPATH"
 python ./core_model/CRUL.py --uni_name CRUL_full --model wideresnet50 --dataset pet-37 --learning_rate 2e-4 --optimizer adam --batch_size 256 --noise_type asymmetric --noise_ratio 0.5 --repair_iter_num 5 --num_epochs 5 --ul_epochs 3 --agree_epochs 3 --mixup_alpha 0.75 --ls_gamma=0.2 --temperature=0.8
 
 # w/o Unlearning
-python ./core_model/CRUL.py --uni_name CRUL_ul --model wideresnet50 --dataset pet-37 --learning_rate 2e-4 --optimizer adam --batch_size 256 --noise_type asymmetric --noise_ratio 0.5 --repair_iter_num 5 --num_epochs 5 --ul_epochs 0 --agree_epochs 3 --mixup_alpha 0.75 --ls_gamma=0.2 --temperature=0.8
+python ./core_model/CRUL.py --uni_name CRUL_wo_ul --model wideresnet50 --dataset pet-37 --learning_rate 2e-4 --optimizer adam --batch_size 256 --noise_type asymmetric --noise_ratio 0.5 --repair_iter_num 5 --num_epochs 5 --ul_epochs 0 --agree_epochs 3 --mixup_alpha 0.75 --ls_gamma=0.2 --temperature=0.8
 
 # w/o High-confidence agreement label smoothing
 python ./core_model/CRUL.py --uni_name CRUL_wo_ls --model wideresnet50 --dataset pet-37 --learning_rate 2e-4 --optimizer adam --batch_size 256 --noise_type asymmetric --noise_ratio 0.5 --repair_iter_num 5 --num_epochs 5 --ul_epochs 3 --agree_epochs 0 --mixup_alpha 0.75 --ls_gamma=0.2 --temperature=0.8
